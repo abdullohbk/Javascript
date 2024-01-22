637697a94cc704ae0cbfc45b0ff8bf6ba2cdbc54
abdullohbk <tursunovabdulloh858@gmail.com>
2024-01-22 21:32:16 +0500

    plus 10 - lesson

--- app.js
+++ app.js
@@ -1194,3 +1194,187 @@
 // }
 
 // console.log(amalar(7, 5, "*"));
+
+// function arrayNumber(n){
+//     let arr = []
+//     for(let i= 1; i <= n; i++){
+//         if(i % 3 == 0 && i % 5 == 0){
+//              arr.push(i);
+//         }
+       
+//     }
+//     return arr
+// }
+
+// let n = 100;
+// let result = arrayNumber(n)
+
+// console.log(result)
+
+// function arrayNumber(n){
+//     let arr = []
+
+//     for(let i = 0; i < n; i++){
+//         arr.push(i*10);
+//     }
+//     console.log(arr);
+
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         if(i % 2 == 1){
+//             sum += arr[i]
+//             console.log(arr[i])
+//         }
+//     }
+
+//     return sum
+
+// }
+
+// let result = arrayNumber(6)
+// console.log(result)
+
+
+// 10 - DARS /////////////////////////////////////////////////////////
+
+// 4 - masala
+
+// function Summa(n){
+//         let arr = []
+//         for(let  i = 0; i <= n; i++){
+//             arr.push(i*10)
+//         }
+
+//         let summa = 0
+
+//         for(let i = 0; i < arr.length; i++){
+//             if(i % 2 == 1 ){
+//                 summa += arr[i]
+//                 console.log(arr[i]);
+//             }
+//         }
+
+//         return summa
+// }
+
+// let result = Summa(6)
+
+
+// 5 - masala
+
+// function addNumbers(n){
+//     let arr = []
+//     for(let i = 0; i < n; i++){
+//         arr.push(i)
+//     }
+
+//     return arr
+// }
+
+// let result = addNumbers(5)
+// console.log(result)
+
+
+//  6- masala
+
+
+// function addNumbers(n) {
+//   let arr = [];
+//   for (let i = n; i >= 0; i--) {
+//     arr.push(i);
+//   }
+
+//   return arr;
+// }
+
+// let result = addNumbers(5);
+// console.log(result);
+
+
+
+//  7 - masala
+
+    // let a = []
+    // let b = []
+    // let c = []
+    // a["ismi"] = "Abdulloh";
+    // a["yoshi"] = 20;
+    // b["ismi"] = "Elbek";
+    // b["yoshi"] = 20;
+
+    // console.log(
+    //   `${a["ismi"]} ni ${b["ismi"]} dan yosh farqi ${
+    //     Math.abs(a["yoshi"]) - (b["yoshi"])
+    //   }`
+    // );
+
+
+    //  12 - masala
+// function devision(n){
+    
+//     let arr = [];
+//     let sum = 0;
+//     for (let i = 0; i <= n; i++) {
+//       sum += i;
+//     }
+//     sum = sum / 2
+//     return sum
+// }
+
+// let result = devision(7)
+// console.log(result)
+
+
+//  13 - masala
+
+// function Number(n){
+//     let arr = []
+//     for(let i = 0; i <= n; i++){
+//         if(i % 2 == 1)[
+//             arr.push(i)
+//         ]
+//     }
+//     return arr
+// }
+
+// let result = Number(9);
+// console.log(result)
+
+// 14 - masala
+
+// function Number(n) {
+//   let arr = [];
+// //   let dev = 0;
+//   for (let i = 0; i <= n; i++){
+//     for(let j = 0; j <= i; j++){
+//         if(i % j == 0 ){
+//             arr.push(j);
+//         }
+//     }
+//   }
+  
+//   return arr
+   
+// }
+
+// // let result = Number(9);
+// // console.log(result);
+
+//  15 - masala
+
+// function Number(n) {
+//   let arr = [];
+//   for (let i = 0; i <= n; i++) {
+//     if (i % 2 == 1) [arr.push(i)];
+//   }
+//   return arr;
+// }
+
+// let result = Number(9);
+// console.log(result);
+
+
+
+
+
+
